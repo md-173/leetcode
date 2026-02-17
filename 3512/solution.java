f92770021ec48d42cc806d635d2888446518bcf0
md-173 <durkanmick@gmail.com>
2026-02-17 21:52:05 +0800

    Reorganized

--- 3512/solution.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int minOperations(int[] nums, int k) {
-        int sum = 0;
-        for(int num : nums) {
-            sum += num;
-        }
-        return sum % k;
-    }
-}
