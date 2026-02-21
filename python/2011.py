aeeba9a6acd1c9624e210ff5956f533eab44a133
md-173 <durkanmick@gmail.com>
2026-02-21 23:24:55 +0800

    Completed problem 2011 in python

---
+++ python/2011.py
@@ -0,0 +1,21 @@
+# 2011. Final Value of Variable After Performing Operations
+# leetcode.com/problems/final-value-of-variable-after-performing-operations
+"""
+There is a programming language with only four operations and one variable X:
+
+++X and X++ increments the value of the variable X by 1.
+--X and X-- decrements the value of the variable X by 1.
+Initially, the value of X is 0.
+
+Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
+
+"""
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        x = 0
+        for op in operations:
+            if op[1] == "-":
+                x -= 1
+            if op[1] == '+':
+                x += 1
+        return x
