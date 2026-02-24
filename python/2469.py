5bd0ff3b283d1926ecf77577bd284347d5185e37
md-173 <durkanmick@gmail.com>
2026-02-24 22:14:28 +0800

    Completed problem 2469 in python

---
+++ python/2469.py
@@ -0,0 +1,13 @@
+"""
+2469. Convert the Temperature
+You are given a non-negative floating point number rounded to two decimal places celsius, that denotes the temperature in Celsius.
+
+You should convert Celsius into Kelvin and Fahrenheit and return it as an array ans = [kelvin, fahrenheit].
+
+Return the array ans. Answers within 10-5 of the actual answer will be accepted.
+"""
+# leetcode.com/problems/convert-the-temperature/
+
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        return [celsius + 273.15, celsius * 1.8 + 32]
