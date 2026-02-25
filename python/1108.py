29a1e8eb7711795d5380883d61fbd2e4a84aff75
md-173 <durkanmick@gmail.com>
2026-02-25 23:23:43 +0800

    Completed 1108 python

---
+++ python/1108.py
@@ -0,0 +1,18 @@
+'''
+1108. Defanging an IP Address
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+A defanged IP address replaces every period "." with "[.]".
+
+'''
+# leetcode.com/problems/defanging-an-ip-address/
+
+class Solution(object):
+    def defangIPaddr(self, address: str) -> str:
+        return address.replace('.','[.]')
+        
