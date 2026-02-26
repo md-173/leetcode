354dd393a13e351f48621dffcab3d29889c4443e
md-173 <durkanmick@gmail.com>
2026-02-26 21:33:44 +0800

    Completed problem 3289 python

---
+++ python/3289.py
@@ -0,0 +1,19 @@
+'''
+3289. The Two Sneaky Numbers of Digitville
+In the town of Digitville, there was a list of numbers called nums containing integers from 0 to n - 1. Each number was supposed to appear exactly once in the list, however, two mischievous numbers sneaked in an additional time, making the list longer than usual.
+
+As the town detective, your task is to find these two sneaky numbers. Return an array of size two containing the two numbers (in any order), so peace can return to Digitville.
+'''
+# leetcode.com/problems/the-two-sneaky-numbers-of-digitville
+
+class Solution:
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        seen = [False] * n
+        res = []
+        for num in nums:
+            if seen[num]:
+                res.append(num)
+            else:
+                seen[num] = True
+        return res
