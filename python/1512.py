252e64b61f239019ca84d330b544562f6078920e
md-173 <durkanmick@gmail.com>
2026-02-27 22:02:27 +0800

    Completed problem 1512 in python

---
+++ python/1512.py
@@ -0,0 +1,15 @@
+'''
+1512. Number of Good Pairs
+Given an array of integers nums, return the number of good pairs.
+
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+'''
+# leetcode.com/problems/number-of-good-pairs/
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        count = 0
+        for i in range(0,len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j] and i < j:
+                    count += 1
+        return count
