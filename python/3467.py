c3e2b7a67c7e042f2739f044258a74ef540a4b82
md-173 <durkanmick@gmail.com>
2026-02-28 21:56:07 +0800

    Completed problem 3467 in python

---
+++ python/3467.py
@@ -0,0 +1,20 @@
+'''
+3467. Transform Array by Parity
+You are given an integer array nums. Transform nums by performing the following operations in the exact order specified:
+
+Replace each even number with 0.
+Replace each odd numbers with 1.
+Sort the modified array in non-decreasing order.
+Return the resulting array after performing these operations.
+https://leetcode.com/problems/transform-array-by-parity
+'''
+class Solution:
+    def transformArray(self, nums: List[int]) -> List[int]:
+        r = []
+        for num in nums:
+            if num % 2 == 0:
+                r.append(0)
+            else:
+                r.append(1)
+        r.sort()
+        return r
