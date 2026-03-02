214260d1f06556cf48bae9276563ddfaf5f5fd32
md-173 <durkanmick@gmail.com>
2026-03-02 23:20:16 +0800

    Completed problem 771 python

---
+++ python/3794.py
@@ -0,0 +1,13 @@
+'''
+3794. Reverse String Prefix
+You are given a string s and an integer k.
+
+Reverse the first k characters of s and return the resulting string.
+https://leetcode.com/problems/reverse-string-prefix/
+'''
+class Solution:
+    def reversePrefix(self, s: str, k: int) -> str:
+        first = s[:k]
+        end = s[k:]
+        rev = first[::-1]
+        return rev + end
