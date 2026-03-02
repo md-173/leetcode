06844ac912fb78f5e29a72938d6a9909157176f6
md-173 <durkanmick@gmail.com>
2026-03-02 23:20:50 +0800

    Fixed 771 python comments

--- python/771.py
+++ python/771.py
@@ -1,11 +1,5 @@
 '''
 771. Jewels and Stones
-Solved
-Easy
-Topics
-premium lock icon
-Companies
-Hint
 You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
 
 Letters are case sensitive, so "a" is considered a different type of stone from "A".
