52529958be3a266b48cc73a4cea738cd7916839b
md-173 <durkanmick@gmail.com>
2026-03-07 23:15:33 +0800

    Completed 1470 python

---
+++ python/1470.py
@@ -0,0 +1,14 @@
+'''
+1470. Shuffle the Array
+Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+'''
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        j = [0] * (2 * n)
+        for i in range(0, n):
+            j[2*i] = nums[i]
+            j[2*i + 1] = nums[i+n]
+        return j
