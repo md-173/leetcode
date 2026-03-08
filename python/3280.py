a37afb6fd3ee018d3b3ab55ff4cf15561909f71c
md-173 <durkanmick@gmail.com>
2026-03-08 15:16:41 +0800

    Completed 3280 python

---
+++ python/3280.py
@@ -0,0 +1,14 @@
+'''
+280. Convert Date to Binary
+You are given a string date representing a Gregorian calendar date in the yyyy-mm-dd format.
+
+date can be written in its binary representation obtained by converting year, month, and day to their binary representations without any leading zeroes and writing them down in year-month-day format.
+
+Return the binary representation of date.
+
+ 
+https://leetcode.com/problems/convert-date-to-binary
+'''
+class Solution:
+    def convertDateToBinary(self, date: str) -> str:
+        return "-".join(f"{int(d):b}" for d in date.split("-"))
