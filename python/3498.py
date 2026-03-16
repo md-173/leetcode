4b8326f482e5687013f053eb4e5b4491d235c23f
md-173 <durkanmick@gmail.com>
2026-03-16 23:00:58 +0800

    complete problem 3498 in python

---
+++ python/3498.py
@@ -0,0 +1,16 @@
+'''
+3498. Reverse Degree of a String
+Given a string s, calculate its reverse degree.
+
+The reverse degree is calculated as follows:
+
+For each character, multiply its position in the reversed alphabet ('a' = 26, 'b' = 25, ..., 'z' = 1) with its position in the string (1-indexed).
+Sum these products for all characters in the string.
+Return the reverse degree of s.
+'''
+class Solution:
+    def reverseDegree(self, s: str) -> int:
+        rd = 0
+        for i in range(0, len(s)):
+            rd += (26 - (ord(s[i]) - 97)) * (i+1)
+        return rd
